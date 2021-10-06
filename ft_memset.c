31b070a39c47933912dd283228b73b6ee628f36b
Sarina Jannet <sjannet@pr-d5.kzn.21-school.ru>
2021-10-06 20:13:30 +0300

    first day

---
+++ ft_memset.c
@@ -0,0 +1,9 @@
+#include <libft.h>
+
+void	*memset (void *destination, int c, size_t n)
+{
+	char	c;
+
+	c = *str;
+	return (c);
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include 'ft_isaplha.c'
+
+int main()
+{
+    print(ft_isalpha('b'));
+    return (0);
+}
