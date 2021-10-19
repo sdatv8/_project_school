6da8d83cd11deffb8f4cdd03df1113012f05e802
sdatv8 <sdatv8@mail.ru>
2021-10-19 22:36:43 +0300

    secondd part

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,17 +1,46 @@
 #include "libft.h"
+#include <stdio.h>
 
-ft_countword()
+int		count_word(char *s, char c)
 {
-	char	*
+	int		i;
+	int		len;
+
+	i = 0;
+	len = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+		{
+			printf("1\n");
+			i++;
+		}
+			
+		while (s[i] && s[i] != c)
+		{
+			printf("2\n");
+			i++;
+		}
+		len++;
+	}
+	printf("len %d", len);
+	return (len);
 }
 
 
-char **ft_split(char const *s, char c)
+// char	**ft_split(char const *s, char c)
+// {
+// 	char	**tmp;
+// 	int		i;
+
+// 	i = 0;
+// 	**tmp = (char **)malloc(sizeof(char) * (count_word(s, c) + 1));
+
+// }
+
+int main ()
 {
-	char	**tmp;
-	int		i;
-
-	i = 0;
-	
-
+	char	str[100] = "   Hello,   Flavio Wuensche!  ";
+	char	c = ' ';
+	count_word(str, c);
 }
