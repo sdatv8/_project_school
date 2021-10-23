43c9b92f1443ffbb500a5828999e810f5d4d94bb
Sarina Jannet <sjannet@pr-b1.kzn.21-school.ru>
2021-10-23 18:26:26 +0300

    add libft Make

--- libft/libft.h
+++ libft/libft.h
@@ -1,4 +1,4 @@
-#ifndef LIBFT_H
+# ifndef LIBFT_H
 # define LIBFT_H
 
 #include <unistd.h>
@@ -30,13 +30,17 @@ void	*ft_calloc(size_t count, size_t size);
 char 	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strtrim(char const *s1, char const *set);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 int		ft_nbrlen(int c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 
 # endif
